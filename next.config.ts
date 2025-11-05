aff393b63ca1b44ffe6b9e6fbeb94688eb7d5036
Abdulelah Baroudi <dev.abdulelah1@gmail.com>
2025-11-05 16:58:01 +0300

    fix vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
-import type { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
-
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     remotePatterns: [
       {
